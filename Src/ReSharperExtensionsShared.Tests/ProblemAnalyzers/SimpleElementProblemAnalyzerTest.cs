f176e90e2a44b39bb84d9ac60c366384ddadac1c
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-07-30 00:37:13 +0200

    Add SimpleElementProblemAnalyzer

---
+++ Src/ReSharperExtensionsShared.Tests/ProblemAnalyzers/SimpleElementProblemAnalyzerTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FakeItEasy;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Tree;
+using NUnit.Framework;
+using ReSharperExtensionsShared.ProblemAnalyzers;
+
+namespace ReSharperExtensionsShared.Tests.ProblemAnalyzers
+{
+    [TestFixture]
+    public class SimpleElementProblemAnalyzerTest
+    {
+        private IDeclaration _declaration;
+        private ElementProblemAnalyzerData _data;
+        private IHighlightingConsumer _consumer;
+
+        private TestProblemAnalyzer _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _declaration = A.Fake<IDeclaration>();
+            _data = A.Dummy<ElementProblemAnalyzerData>();
+            _consumer = A.Dummy<IHighlightingConsumer>();
+
+            _sut = A.Fake<TestProblemAnalyzer>(o => o.CallsBaseMethods());
+        }
+
+        [Test]
+        public void Run()
+        {
+            ((IElementProblemAnalyzer) _sut).Run(_declaration, _data, _consumer);
+
+            A.CallTo(() => _sut.PublicRun(_declaration, _declaration.DeclaredElement, _data, _consumer)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Test]
+        public void Run_WithNullDeclaredElement_IsFiltered()
+        {
+            A.CallTo(() => _declaration.DeclaredElement).Returns(null);
+
+            ((IElementProblemAnalyzer) _sut).Run(_declaration, _data, _consumer);
+
+            A.CallTo(_sut).MustNotHaveHappened();
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public abstract class TestProblemAnalyzer : SimpleElementProblemAnalyzer<IDeclaration, IDeclaredElement>
+        {
+            protected override void Run(
+                IDeclaration declaration,
+                IDeclaredElement declaredElement,
+                ElementProblemAnalyzerData data,
+                IHighlightingConsumer consumer)
+            {
+                PublicRun(declaration, declaredElement, data, consumer);
+            }
+
+            [SuppressMessage("ReSharper", "UnusedParameter.Global")]
+            public virtual void PublicRun(
+                IDeclaration declaration,
+                IDeclaredElement declaredElement,
+                ElementProblemAnalyzerData data,
+                IHighlightingConsumer consumer)
+            {
+            }
+        }
+    }
+}
---
+++ Src/ReSharperExtensionsShared/ProblemAnalyzers/SimpleElementProblemAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace ReSharperExtensionsShared.ProblemAnalyzers
+{
+    public abstract class SimpleElementProblemAnalyzer<TDeclaration, TDeclaredElement> : ElementProblemAnalyzer<TDeclaration>
+        where TDeclaration : IDeclaration
+        where TDeclaredElement : IDeclaredElement
+    {
+        protected sealed override void Run(TDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            var declaredElement = element.DeclaredElement;
+
+            if (declaredElement != null)
+            {
+                Run(element, (TDeclaredElement) declaredElement, data, consumer);
+            }
+        }
+
+        protected abstract void Run(
+            [NotNull] TDeclaration declaration,
+            [NotNull] TDeclaredElement declaredElement,
+            [NotNull] ElementProblemAnalyzerData data,
+            [NotNull] IHighlightingConsumer consumer);
+    }
+}
