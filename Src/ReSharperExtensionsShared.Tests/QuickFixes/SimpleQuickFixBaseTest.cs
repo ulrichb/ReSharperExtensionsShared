0cce970823655e9d8b894069592b8b94602e6fbc
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-11-30 03:30:36 +0100

    Add 'SimpleQuickFixBase'

---
+++ Src/ReSharperExtensionsShared.Tests/QuickFixes/SimpleQuickFixBaseTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using FakeItEasy;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.Util;
+using NUnit.Framework;
+using ReSharperExtensionsShared.Highlighting;
+using ReSharperExtensionsShared.QuickFixes;
+
+namespace ReSharperExtensionsShared.Tests.QuickFixes
+{
+    [TestFixture]
+    [TestNetFramework4]
+    public class SimpleQuickFixBaseTest : BaseTestWithSingleProject
+    {
+        [Test]
+        public void IsAvailable()
+        {
+            Test(sut =>
+            {
+                var userDataHolder = A.Fake<IUserDataHolder>();
+
+                var result = sut.IsAvailable(userDataHolder);
+
+                Assert.That(result, Is.EqualTo(true));
+                A.CallTo(sut).Where(x => x.Method.Name == "IsAvailableForTreeNode")
+                    .WhenArgumentsMatch((IUserDataHolder x) => ReferenceEquals(x, userDataHolder)).MustHaveHappened();
+            });
+        }
+
+        [Test]
+        public void IsAvailable_WithInvalidTreeNode()
+        {
+            Test(
+                useNullTreeNode: true,
+                action: sut =>
+                {
+                    var result = sut.IsAvailable(A.Fake<IUserDataHolder>());
+
+                    Assert.That(result, Is.EqualTo(false));
+                });
+        }
+
+        [Test]
+        public void IsAvailable_WithIsAvailableForTreeNodeReturnsFalse()
+        {
+            Test(sut =>
+            {
+                A.CallTo(sut).Where(x => x.Method.Name == "IsAvailableForTreeNode").WithReturnType<bool>().Returns(false);
+
+                var result = sut.IsAvailable(A.Fake<IUserDataHolder>());
+
+                Assert.That(result, Is.EqualTo(false));
+            });
+        }
+
+        private void Test(Action<SimpleQuickFixBase<TestHighlighting, ITreeNode>> action, bool useNullTreeNode = false)
+        {
+            WithSingleProject(
+                GetTestDataFilePath2("SampleClass.cs").FullPath,
+                (lifetime, solution, project) => RunGuarded(() =>
+                {
+                    var primaryPsiFile = project.GetAllProjectFiles().Single().GetPrimaryPsiFile().NotNull();
+
+                    var highlighting = new TestHighlighting(useNullTreeNode ? null : primaryPsiFile);
+
+                    var sut = A.Fake<SimpleQuickFixBase<TestHighlighting, ITreeNode>>(
+                        o => o.WithArgumentsForConstructor(new[] { highlighting }).CallsBaseMethods());
+
+                    A.CallTo(sut).Where(x => x.Method.Name == "IsAvailableForTreeNode").WithReturnType<bool>().Returns(true);
+
+                    action(sut);
+                }));
+        }
+
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public class TestHighlighting : SimpleTreeNodeHighlightingBase<ITreeNode>
+        {
+            public TestHighlighting(ITreeNode treeNode) : base(treeNode, "don't care")
+            {
+            }
+        }
+    }
+}
---
+++ Src/ReSharperExtensionsShared/QuickFixes/SimpleQuickFixBase.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using JetBrains.ReSharper.Feature.Services.QuickFixes;
+using JetBrains.ReSharper.Intentions.Util;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.Util;
+using ReSharperExtensionsShared.Highlighting;
+
+namespace ReSharperExtensionsShared.QuickFixes
+{
+    /// <summary>
+    /// A base class for Quick Fixes which provides <see cref="QuickFixBase.ExecutePsiTransaction"/> for
+    /// valid declarations of <see cref="THighlighting"/>.
+    /// </summary>
+    public abstract class SimpleQuickFixBase<THighlighting, TDeclaration> : QuickFixBase
+        where THighlighting : SimpleTreeNodeHighlightingBase<TDeclaration>
+        where TDeclaration : class, ITreeNode
+    {
+        [NotNull]
+        [PublicAPI]
+        protected readonly THighlighting Highlighting;
+
+        protected SimpleQuickFixBase([NotNull] THighlighting highlighting)
+        {
+            Highlighting = highlighting;
+        }
+
+        public override bool IsAvailable(IUserDataHolder cache) => IsTreeNodeValid && IsAvailableForTreeNode(cache);
+
+        [PublicAPI]
+        protected abstract bool IsAvailableForTreeNode([NotNull] IUserDataHolder cache);
+
+        private bool IsTreeNodeValid => ValidUtils.Valid(Highlighting.TreeNode);
+    }
+}
